81ea4cd8fbed18aa7be7aee41d704fc4a898c6ea
Justine Rhei Torres <justinerheitorres@gmail.com>
2026-02-09 22:41:17 +0800

    chore: used JS date instead of static year definition

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -4,13 +4,14 @@ import { useTheme } from '@/contexts/ThemeContext';
 
 const Footer = () => {
   const { darkMode } = useTheme();
+  const currentDate = new Date()
 
   return (
     <footer className={`${darkMode ? 'bg-gray-900' : 'bg-white'}`}>
       <div className="border-t border-gray-200 dark:border-gray-700">
         <div className="container mx-auto px-4 py-8">
           <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
-            <p className={`text-sm ${darkMode ? 'text-gray-300' : 'text-gray-600'}`}>© 2025 Justine Rhei Torres. All rights reserved.</p>
+            <p className={`text-sm ${darkMode ? 'text-gray-300' : 'text-gray-600'}`}>© {currentDate.getFullYear()} Justine Rhei Torres. All rights reserved.</p>
             <p className={`text-sm ${darkMode ? 'text-gray-300' : 'text-gray-600'}`}>Built with Next.js &amp; Shadcn</p>
           </div>
         </div>
